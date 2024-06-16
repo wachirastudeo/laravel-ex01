905946734ba556b3c925dc256453d659df02f544
wachirastudeo <wachirastudeo@gmail.com>
2024-06-16 13:40:07 +0700

    create controller admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    function index()
+    {
+        $blogs = [
+            [
+                'title' => 'บทความที่ 1',
+                'content' => 'เนื้อหาบทความที่ 1',
+                'status' => true
+            ],
+            [
+                'title' => 'บทความที่ 2',
+                'content' => 'เนื้อหาบทความที่ 2',
+                'status' => true
+            ],
+            [
+                'title' => 'บทความที่ 3',
+                'content' => 'เนื้อหาบทความที่ 3',
+                'status' => false
+            ],
+        ];
+        return view('blog', compact('blogs'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('blog', function () {
-    $blogs = [
-        [
-            'title' => 'บทความที่ 1',
-            'content' => 'เนื้อหาบทความที่ 1',
-            'status' => true
-        ],
-        [
-            'title' => 'บทความที่ 2',
-            'content' => 'เนื้อหาบทความที่ 2',
-            'status' => true
-        ],
-        [
-            'title' => 'บทความที่ 3',
-            'content' => 'เนื้อหาบทความที่ 3',
-            'status' => false
-        ],
-    ];
-    return view('blog', compact('blogs'));
-})->name('blog');
+Route::get('blog', [AdminController::class, 'index'])->name('blog');
