fa42030e75b7d48725bae1c1ea04651710704af5
wachirastudeo <wachirastudeo@gmail.com>
2024-06-16 10:42:50 +0700

    custom welcompage

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>บทความทั้งหมด</title>
+</head>
+<body>
+    <h2>บทความทั้งหมด</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor tempora nesciunt explicabo necessitatibus sed vitae nostrum sint eum. Et, omnis voluptas. Dolorem, corrupti! Ut molestiae, sapiente iure at nihil reprehenderit.</p>
+    
+</body>
+</html>
