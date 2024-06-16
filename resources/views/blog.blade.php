65ba791c2173031dc0d7f43c2f77e35b1839fd08
wachirastudeo <wachirastudeo@gmail.com>
2024-06-16 13:29:54 +0700

    use if else

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,5 +7,14 @@
 @foreach ($blogs as $item)
        <h4> {{$item['title']}} </h4>
        <p> {{$item['content']}} </p>
+       @if ($item['status']=='true')
+
+       <p class="text text-success">เผยแพร่</p>
+           
+       @else
+       <p class="text text-danger">ฉบับร่าง</p>
+
+           
+       @endif
 @endforeach
 @endsection
