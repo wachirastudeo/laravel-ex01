944ad18f868051c2c5b32f75115fd739b6aefab2
wachirastudeo <wachirastudeo@gmail.com>
2024-06-16 11:28:24 +0700

    send data array with compact

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,5 +3,9 @@
 @section('title','บทความทั้งหมด')
 
 @section('content')
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sequi, tempore quos ullam possimus, rem maxime similique consectetur quis beatae rerum dolorem? Nostrum enim culpa officia quae fugit veniam repellat.
+    <hr>
+@foreach ($blogs as $item)
+       <h4> {{$item['title']}} </h4>
+       <p> {{$item['content']}} </p>
+@endforeach
 @endsection
