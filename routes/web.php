cc3b8504ca40bc4fae3c5addde0c8aaa70a52b67
wachirastudeo <wachirastudeo@gmail.com>
2024-06-17 16:32:05 +0700

    install authen bye laravel ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('delete/{id}', [AdminController::class, 'delete'])->name('delete');
 Route::get('change/{id}', [AdminController::class, 'change'])->name('change');
 Route::get('edit/{id}', [AdminController::class, 'edit'])->name('edit');
 Route::post('update/{id}', [AdminController::class, 'update'])->name('update');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
