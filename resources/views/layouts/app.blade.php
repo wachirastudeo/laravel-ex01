c0654795b521c15923fb7cedbf616afdc07dbea3
wachirastudeo <wachirastudeo@gmail.com>
2024-06-17 18:12:18 +0700

    use layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Blog application
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -81,8 +81,11 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="container py-5">
+
             @yield('content')
+
+
         </main>
     </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
-@extends('layout')
-@section('title','หน้าแรกเว็บไซต์')
-    
+@extends('layouts.app')
+@section('title', 'หน้าแรกเว็บไซต์')
+
 
 @section('content')
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestiae consequuntur repudiandae eaque quisquam, omnis sequi consectetur deleniti enim, minus neque ipsa illo illum quibusdam repellat fugit in molestias repellendus?
+    <h2>หน้าแรกเว็บไซต์</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestiae consequuntur repudiandae eaque quisquam,
+    omnis sequi consectetur deleniti enim, minus neque ipsa illo illum quibusdam repellat fugit in molestias repellendus?
 @endsection
